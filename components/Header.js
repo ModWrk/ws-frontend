dc07f2464b0067233bdcea944a3f9f4b62d50807
ModWrk <erik@modwrk.com>
2018-10-17 12:56:38 -0600

    initial theming

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
-import Nav from './Nav'
+import Nav from './Nav';
+import Link from 'next/link';
 import Router from 'next/router';
+import styled from 'styled-components';
 import NProgress from 'nprogress';
 
 Router.onRouteChangeStart = () => {
@@ -12,11 +14,23 @@ Router.onRouteChangeError = () => {
     console.log('onRouteChangeError Triggered')
 }
 
+const Logo = styled.h1`
+    font-size: 4rem;
+    margin-left: 2rem;
+    position: relative;
+    z-index: 2;
+    transform: skew(-7deg);
+`;
+
 
 const Header = () => (
     <div>   
         <div className="bar">
-            <a href="">Mod Wrk</a>
+            <h1>
+                <Link>
+                    <a href="">Mod Wrk</a>
+                </Link>
+            </h1>
             <Nav />
         </div>
         <div className="sub-bar">
--- components/Page.js
+++ components/Page.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
-// import styled,{ ThemeProvider, injectGlobal} from 'styled-components';
+import styled,{ ThemeProvider, injectGlobal} from 'styled-components';
 
-// const theme = {
-//     black: '#29323F',
-     
-// }
+const theme = {
+    black: '#1b222d',
+    midGrey: '#a8a8a8',
+    ltGrey: '#e5e5e5',
+    drkBlue: '#1d2f3a',
+    midBlue: '#3b4c59',
+    ltBlue: '#576270',
+    primaryBlue: '#2d5b8c',
+    red: '#ef3a34',
+    green: '#e3efcd',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0, 0, 0, 0,.09)',
+};
 
 const StyledPage = styled.div`
-    background: white;
+    max-width: ${props => props.theme.maxWidth};
     color: black;
 
 `;
@@ -23,7 +32,7 @@ const Inner = styled.div`
 class Page extends Component {
     render() {
         return (
-            <ThemeProvider>
+            <ThemeProvider theme={theme}>
                 <StyledPage>
                     <Inner>
                         <Meta />
