c3f344fef8d352ae66964c096a04a0125e23569c
ModWrk <erik@modwrk.com>
2018-10-17 10:15:21 -0600

    Added test styled components

--- components/Page.js
+++ components/Page.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled,{ ThemeProvider, injectGlobal} from 'styled-components';
+
+const theme = {
+    black: '#29323F',
+    
+}
+
+const StyledPage = styled.div`
+    background: white;
+    color: black;
+
+`;
+
+const Inner = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 2rem;
+`;
 
 class Page extends Component {
     render() {
         return (
-            <div>
-                <Meta />
-                <Header />
-                {this.props.children}
-            </div>
+            <StyledPage>
+                <Inner>
+                    <Meta />
+                    <Header />
+                    {this.props.children}
+                </Inner>
+            </StyledPage>
         );
     }
 }
