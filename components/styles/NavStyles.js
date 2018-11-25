011a6c00f7790f441f64ac47885aebfeef421948
ModWrk <erik@modwrk.com>
2018-11-24 21:54:50 -0700

    GraphQL Yoga set up complete ahead of connecting React to GQL

--- components/styles/NavStyles.js
+++ components/styles/NavStyles.js
@@ -5,16 +5,16 @@ const NavStyles = styled.ul`
   padding: 0;
   display: flex;
   justify-self: end;
-  font-size: 2rem;
+  font-size: 1.25rem;
   a,
   button {
-    padding: 1rem 3rem;
+    padding: .75rem 2rem;
     display: flex;
     align-items: center;
     position: relative;
     text-transform: uppercase;
-    font-weight: 900;
-    font-size: 1em;
+    font-weight: 600;
+    font-size: .75em;
     background: none;
     border: 0;
     cursor: pointer;
